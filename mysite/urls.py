5bf6224a7a06a68e52b16bbf231d5e6e0ab0d70c
hmansu444 <hmansusoni444@gmail.com>
2017-05-06 15:07:12 +0530

    comment method and authorization added

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
+    # url(r'^accounts/login/$', views.login),
+    url(r'^accounts/login/$', views.login, name='login'),
 ]
